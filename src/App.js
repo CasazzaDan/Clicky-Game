aad52b61f5e2f4d699aae94f502a34cabc9dcfea
DCas973 <dan.casazza@endeveran.com>
2019-03-25 18:36:45 -0500

    app set up and functioning, ready to get deployed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+import cards from "./cards.json";
 
-function App () {
-  return  "component"
+class App extends Component {
+  // Setting this.state.cards to the cards json array
+  state = {
+    cards,
+    score: 0,
+    highscore: 0
+  };
+//Function wont trigger on condition
+  gameWin = () => {
+    alert(`You Have a Great Memory!! \nscore: ${this.state.score}`);
+    console.log(this.state.score);
+    if(this.state.score === 12) {
+      this.setState({highscore: this.state.highscore}, function() {
+        console.log(this.state.highscore);
+      });
+      this.state.cards.forEach(card => {
+        card.count = 0;
+      });
+      alert(`You Have a Great Memory!! \nscore: ${this.state.score}`);
+      this.setState({score: 0});
+      return true;
+    }
+    else {
+      this.gameOver();
+    }
+  }
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      });
+    }
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.cards.find((o, i) => {
+      if (o.id === id) {
+        if(cards[i].count === 0){
+          cards[i].count = cards[i].count + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true; 
+        } else {
+          this.gameWin();
+        }
+      }
+    });
+  }
+  // Map over this.state.cards and render a cardCard component for each card object
+  render() {
+    return (
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        {this.state.cards.map(card => (
+          <Card
+            clickCount={this.clickCount}
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
